9637d3537e220f4890dd0522748f119f70bbf45b
John Bresnahan <buzztroll@gmail.com>
2025-05-13 20:28:13 +0100

    upgrade HX711

--- printem.py
+++ printem.py
@@ -29,6 +29,5 @@ if __name__ == '__main__':
         rc = main()
         sys.exit(rc)
     finally:
-        print("relay off exit")
-        relay.off()
+        print("off exit")
 
--- welcome.sh
+++ welcome.sh
@@ -3,7 +3,9 @@
 #mpg123 /home/bresnaha/grinch.mp3 &
 #mpg123 /home/bresnaha/scary-welcome.mp3 &> /dev/null
 #sudo  /home/bresnaha/Dev/cottage/.venv/bin/buzz-welcome-lights &
+
 . /home/bresnaha/Dev/cottage/.venv/bin/activate
-#sudo -E /home/bresnaha/Dev/cottage/.venv/bin/python /home/bresnaha/Dev/buzzstep/lights1.py
+/home/bresnaha/Dev/SLACK/slack-photo-uploader/send-snap.sh &
+#sudo -E /home/bresnaha/Dev/cottage/.venv/bin/python /home/bresnaha/Dev/buzzstep/lights1.py &
 sleep 60
 wait
